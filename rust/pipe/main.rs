8ea3310f79b4e25f580c8b0f2a19f9cb962c866f
AndyLu <laserlue@gmail.com>
2023-05-04 09:12:37 +0800

    feat(python): adapter pattern
    Adapter pattern

--- rust/pipe/main.rs
+++ rust/pipe/main.rs
@@ -1,8 +1,6 @@
 // Pipe
 // ref:
 // 1. https://plippe.github.io/blog/2021/06/09/rust-extension-methods.html
-use std::fmt::Debug;
-
 fn main() {
     "Hello, World"
         .pipe(|s| {
@@ -10,23 +8,6 @@ fn main() {
             return s;
         })
         .pipe(|s| println!("{s} [second pipe]"));
-}
-
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u32,
-    address: String,
-}
-
-impl Person {
-    fn new() -> Self {
-        Person {
-            name: "andy".to_string(),
-            age: 18,
-            address: "test".to_string(),
-        }
-    }
 }
 
 // Pipe
